37c17fbc2696fbdf2ab5013eae4b887737cbb0c3
matt.todd <matt.todd@asolutions.com>
2011-12-16 06:46:38 -0600

    added multiple file content types

--- web.js
+++ web.js
@@ -7,6 +7,16 @@ var PATH = require('path')
 
 var WEBROOT = "./webroot"
 
+var MIME_TYPES = {
+   default: 'text/plain',
+   html: 'text/html',
+   js: 'application/javascript',
+   css: 'text/css',
+   gif: 'image/gif',
+   jpg: 'image/jpeg',
+   png: 'image/png',
+}
+
 WEB.createServer(function(req, res) {
    todd.fileServer.handleFileRequest(req.url, res)
 }).listen(1337, '127.0.0.1')
@@ -32,6 +42,21 @@ todd.fileServer.handleFileRequest = function (url, res) {
 todd.fileServer.serveFile = function (url, path, res) {
    console.log('HTTP 200 ' + url)
 
-   res.writeHead(200, {'Content-type': 'text/plain'})
-   FS.createReadStream(path).pipe(res)
+   FS.stat(path, function(err, stats) {
+      res.writeHead(200, {
+         'Content-type': todd.fileServer.mimeType(path),
+         'Content-length': stats.size
+      })
+      FS.createReadStream(path).pipe(res)
+   })
+}
+
+todd.fileServer.mimeType = function(path) {
+   var extension = path.substring(path.lastIndexOf('.') + 1)
+   var contentType = MIME_TYPES[extension]
+   if (!contentType) {
+      contentType = MIME_TYPES['default']
+   }
+   console.log(extension + ' => ' + contentType)
+   return contentType
 }
