7c631bf9d258f2748683df319f77592546d7909e
matt.todd <matt.todd@asolutions.com>
2011-12-23 14:06:42 -0600

    now a full CRUD app using mongodb

--- README.md
+++ README.md
@@ -14,7 +14,9 @@
 
 ## install pre-requisites
 ```$ sudo apt-get install -y nodejs npm
-$ npm install express```
+$ npm install express
+$ npm install mongoose
+$ npm install ejs```
 
 ## start server                 
 ```$ node app.js```
--- express-app/app.js
+++ express-app/app.js
@@ -1,9 +1,19 @@
-var express = require('express')
+var util = require('util');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/sample1');
+var PersonSchema = new mongoose.Schema({
+      firstName: String,
+      lastName: String
+   });
+var Person = mongoose.model('Person', PersonSchema);
+
+var express = require('express');
 var app = express.createServer();
 app.configure(function() {
    this.set('view engine', 'ejs');
    this.use(express.bodyParser());
    this.use(express.methodOverride());
+   this.use(express.static(__dirname + '/public'));
 });
 app.listen(1337);
 
@@ -16,35 +26,55 @@ app.get('/people', function(req, res){
 });
 
 showAllPeople = function(req, res) {
-   var peopleList = [
-               {id: 1, firstName: 'Matt', lastName: 'Todd'},
-               {id: 2, firstName: 'Abbi', lastName: 'Todd'},
-               {id: 3, firstName: 'Caleb', lastName: 'Todd'},
-               {id: 4, firstName: 'Aurelia', lastName: 'Todd'}
-            ];
-   res.render('people/index', {people: peopleList});
+   Person.find(function(err, docs) {
+      docs.each(function(entity) {
+         console.log(util.inspect(entity));
+      });
+      res.render('people/index', {people: docs});
+   });
 }
 
 app.get('/people/new', function(req, res){
    res.render('people/create');
 });
 
+app.delete('/people/:id', function(req, res){
+   var id = req.params.id;
+   console.log('deleting person: ' + id);
+   Person.findById(id, function(err, doc) {
+      doc.remove(function(err) {
+         res.send({});
+      });
+   });
+});
+
 app.get('/people/:id', function(req, res){
    var id = req.params.id;
-   var person = {id: 1, firstName: 'Matt', lastName: 'Todd'};
-   res.render('people/edit', {person: person });
+   Person.findById(id, function(err, doc) {
+      res.render('people/edit', {person: doc});
+   });
 });
 
 app.post('/people', function(req, res){
-   var person = req.body.person;
-   console.log('creating: ' + person);
-   showAllPeople(req, res);
+   var person = new Person();
+   person.firstName = req.body.person.firstName;
+   person.lastName = req.body.person.lastName;
+   person.save(function(err) {
+      console.log('creating: ' + util.inspect(person));
+      showAllPeople(req, res);
+   });
 });
 
 app.put('/people', function(req, res){
-   var person = req.body.person;
-   console.log('updating: ' + person);
-   showAllPeople(req, res);
+   var id = req.body.person.id;
+   Person.findById(id, function(err, doc) {
+      doc.firstName = req.body.person.firstName;
+      doc.lastName = req.body.person.lastName;
+      doc.save(function(err) {
+         console.log('updating: ' + doc);
+         showAllPeople(req, res);
+      });
+   });
 });
 
 
@@ -56,3 +86,4 @@ if (!Array.prototype.each) {
       }
    }
 }
+
