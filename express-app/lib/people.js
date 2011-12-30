d8cf3df52154251254e96c60f2202000dda05b58
matt.todd <matt.todd@asolutions.com>
2011-12-30 11:44:04 -0600

    added some UI tweaks

--- express-app/lib/people.js
+++ express-app/lib/people.js
@@ -3,24 +3,21 @@ var mongoose = require('mongoose');
 
 var PersonSchema = new mongoose.Schema({
       firstName: String,
-      lastName: String
+      lastName: String,
+      middleName: String
    });
 var Person = mongoose.model('Person', PersonSchema);
 
 exports.index = function(req, res){
-   connect();
-
    showAllPeople(req, res);
-
-   disconnect();
 };
 
 function showAllPeople(req, res) {
-   Person.find(function(err, docs) {
-      docs.each(function(entity) {
+   Person.find(function(err, people) {
+      people.each(function(entity) {
          console.log(util.inspect(entity));
       });
-      res.render('people/index', {people: docs});
+      res.render('people/index', {people: people});
    });
 }
 
@@ -29,64 +26,42 @@ exports.new = function(req, res){
 };
 
 exports.destroy = function(req, res){
-   connect();
-
    var id = req.params.person;
    console.log('deleting person: ' + id);
    Person.findById(id, function(err, doc) {
       doc.remove(function(err) {
          res.send({});
-
-         disconnect();
       });
    });
 };
 
 exports.edit = function(req, res){
-   connect();
-
    var id = req.params.person;
    Person.findById(id, function(err, doc) {
       res.render('people/edit', {person: doc});
-
-      disconnect();
    });
 };
 
 exports.create = function(req, res){
-   connect();
-
    var person = new Person();
    person.firstName = req.body.person.firstName;
    person.lastName = req.body.person.lastName;
+   person.middleName = req.body.person.middleName;
    person.save(function(err) {
       console.log('creating: ' + util.inspect(person));
       showAllPeople(req, res);
-
-      disconnect();
    });
 };
 
 exports.update = function(req, res){
-   connect();
-
    var id = req.body.person.id;
    Person.findById(id, function(err, doc) {
       doc.firstName = req.body.person.firstName;
       doc.lastName = req.body.person.lastName;
+      doc.middleName = req.body.person.middleName;
       doc.save(function(err)  {
          console.log('updating: ' + doc);
          showAllPeople(req, res);
-
-         disconnect();
       });
    });
 };
-
-function connect() {
-   mongoose.connect('mongodb://localhost/sample');
-}
-
-function disconnect() {
-   mongoose.disconnect();
-}
