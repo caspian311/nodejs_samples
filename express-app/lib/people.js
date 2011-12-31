0eba3134e1db2557fab305edbba58a7bac9a55b7
matt.todd <matt.todd@asolutions.com>
2011-12-31 07:34:56 -0600

    refactored out a controller for the people actions

--- express-app/lib/people.js
+++ express-app/lib/people.js
@@ -1,60 +1,8 @@
-var mongoose = require('mongoose');
+var peopleController = require('./people_controller');
 
-var PersonSchema = new mongoose.Schema({
-      firstName: String,
-      lastName: String,
-      middleName: String
-   });
-var Person = mongoose.model('Person', PersonSchema);
-
-exports.index = function(req, res){
-   showAllPeople(req, res);
-};
-
-function showAllPeople(req, res) {
-   Person.find(function(err, people) {
-      res.render('people/index', {people: people});
-   });
-}
-
-exports.new = function(req, res){
-   res.render('people/create');
-};
-
-exports.destroy = function(req, res){
-   var id = req.params.person;
-   Person.findById(id, function(err, doc) {
-      doc.remove(function(err) {
-         res.send({});
-      });
-   });
-};
-
-exports.edit = function(req, res){
-   var id = req.params.person;
-   Person.findById(id, function(err, doc) {
-      res.render('people/edit', {person: doc});
-   });
-};
-
-exports.create = function(req, res){
-   var person = new Person();
-   person.firstName = req.body.person.firstName;
-   person.lastName = req.body.person.lastName;
-   person.middleName = req.body.person.middleName;
-   person.save(function(err) {
-      showAllPeople(req, res);
-   });
-};
-
-exports.update = function(req, res){
-   var id = req.body.person.id;
-   Person.findById(id, function(err, doc) {
-      doc.firstName = req.body.person.firstName;
-      doc.lastName = req.body.person.lastName;
-      doc.middleName = req.body.person.middleName;
-      doc.save(function(err)  {
-         showAllPeople(req, res);
-      });
-   });
-};
+exports.index = peopleController.index;
+exports.new = peopleController.new;
+exports.destroy = peopleController.destroy;
+exports.edit = peopleController.edit;
+exports.create = peopleController.create;
+exports.update = peopleController.update;
